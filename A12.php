ae994be2a6bf4416c1d4469af6c3a93842f658c1
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-18 14:47:34 -0500

    cc

---
+++ A12.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Horizonatal Chart</title>
+</head>
+<body>
+  <div>
+    <h2>Student ID : 1001821620<br>Name : Vivek Vishwanath Shetye</h2><br><br><br>
+  </div>
+  </div>
+</body>
+</html>
+
+<?php
+include_once 'connection.php';
+
+// Input data
+$year1 = $_POST['year1'];
+$year2 = $_POST['year2'];
+$stateco = $_POST['stateco'];
+
+$dataPoints = array();
+
+$sql = "SELECT COUNT(totalvotes) AS T1, year FROM ptelect WHERE year BETWEEN $year1 AND $year2 AND state_po='$stateco' GROUP BY year";
+$result = mysqli_query($con, $sql) or die('Error ' . mysqli_error($con));
+
+$count = 0;
+
+while($row = mysqli_fetch_array($result))
+{
+    $dataPoints[$i]["y"] = $row['year'];
+    $dataPoints[$i]["label"] = $row['T1'];
+
+    $count++;
+}
+
+echo "<br><br><br><br><br>";
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="removeWatermark.css">
+<script>
+window.onload = function() {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    title:{
+        text: "Presedential Election"
+    },
+    axisX: {
+        title: "Candidate Count"
+    },
+    axisY: {
+        title: "Year"
+    },
+    data: [{
+        type: "bar",
+        yValueFormatString: "# count",
+        dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+    }]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 100%; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
